7fd748b4d9e1fd7f61113d86e50dbaf53c4d8ffa
ironsketch <michellejbergin@gmail.com>
2021-12-02 22:18:17 -0700

    updated view added more movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,15 @@ export class AppComponent {
     movies: MovieModel[] = [];
     filteredMovies: MovieModel[] = [];
     searchS = '';
+    body = 'body';
+    comedy = 'comedy';
+    folk = 'folk';
+    found_footage = 'found footage';
+    gothic = 'gothic';
+    natural = 'natural';
+    slasher = 'slasher';
+    teen = 'teen';
+    psychological = 'psychological';
     
     constructor(private dataService: DataService) {
         this.dataService.loadMovies().subscribe(response => {
@@ -31,7 +40,11 @@ export class AppComponent {
     search(event: any) {
         if (this.searchS.length >= 3) {
             this.filteredMovies = [];
-            this.filteredMovies = this.movies.filter(movie => movie.title.toLowerCase().includes(this.searchS));
+            this.filteredMovies = this.movies.filter(movie => movie.title.toLowerCase().includes(this.searchS.toLowerCase()));
         }
+    }
+
+    hasData(name: string): boolean {
+        return this.movies.filter(movie => movie.genre?.includes(name)).length > 0;
     }
 }
